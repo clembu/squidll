87cd98fd69aebb3532bf3804a2113693d55a1518
Clément Busschaert <clement.busschaert@outlook.com>
2017-10-31 16:04:57 +0100

    Blaze CSS

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,5 +1,8 @@
 // Initial welcome page. Delete the following line to remove it.
 'use strict';
+import './blaze.min.css';
+document.body.classList.add('c-text');
+
 import * as riot from 'riot';
 import {mount} from "./tags";
 mount({ text: "Hello world!", subtext: "This world welcomes you." });
