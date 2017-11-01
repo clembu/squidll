b72f42b0b843fec134b10b1d408af9b9e422d2e8
Clément Busschaert <clement.busschaert@outlook.com>
2017-11-01 18:17:20 +0100

    Mousetrap shortcuts

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -2,8 +2,13 @@
 'use strict';
 import './blaze.min.css';
 import './font-awesome/css/font-awesome.min.css'
+import mousetrap from 'mousetrap';
 document.body.classList.add('c-text');
 
 import * as riot from 'riot';
 import {mount} from "./tags";
-mount({ text: "Hello world!", subtext: "This world welcomes you." });
+const tags = mount({ text: "Hello world!", subtext: "This world welcomes you." });
+const app = tags[0];
+
+mousetrap.bind(['ctrl+k'], (e,s) => {app.trigger('newknot')})
+mousetrap.bind(['ctrl+s'], (e,s) => {app.trigger('newstitch')})
